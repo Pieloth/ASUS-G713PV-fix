37275b3b8043418098a6481b344dd81b7c9e46ce
Nicolas Martin <73445512+Pieloth@users.noreply.github.com>
2024-06-28 19:36:33 +0200

    Simplify some code

--- ASUS G713PV fix.bat
+++ ASUS G713PV fix.bat
@@ -1,16 +1,5 @@
 @echo off
-:: ASUS ROG tweaks 
-
-set "pausecls=(pause & cls)"
-set quiet=
-set "p1=%1"
-if /I "%p1:~0,2%"=="/q" set quiet=1
-
-:: Retreive legacy active power scheme GUID from 1st line of powercfg /q
-for /f "delims=" %%i in ('powercfg /q') do set actpowplan=%%i & goto :stop
-:stop
-for /f "tokens=2 delims=:" %%i in ("%actpowplan%") do set actpowplan1=%%i
-for /f "tokens=1" %%i in ("%actpowplan1%") do set actpowplanguid=%%i
+:: ASUS ROG G713PV fix 
 
 chcp 65001 > NUL
 
@@ -39,22 +28,35 @@ echo
 echo Use also G Helper instead of Armoury Crate for even better stability
 echo:
 
+set "pausecls=(pause & cls)"
+set quiet=
+set "p1=%1"
+if /I "%p1:~0,2%"=="/q" set quiet=1
+
+:: Retreive legacy active power scheme GUID from 1st line of powercfg /q
+for /f "delims=" %%i in ('powercfg /q') do set actpowplan=%%i & goto :stop
+:stop
+for /f "tokens=2 delims=:" %%i in ("%actpowplan%") do set actpowplan1=%%i
+for /f "tokens=1" %%i in ("%actpowplan1%") do set actpowplanguid=%%i
+
+set "RegKeyHeader=HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control"
+
 :: Get admin status
 if exist %windir%\system32\config\systemprofile\* (
-  echo  ]]] [42m[93m Admin rights ON =^> parameters will be applied[0m
+  echo  ]]] [42m[93m Run with Admin level =^> parameters will be applied[0m
   set admin=1
 ) else (
-  echo  [7m[91m]]] Admin rights OFF =^> No change or parameters will be saved, current and target parameters will be shown[0m
+  echo  [7m[91m]]] Run without Admin level =^> No change or parameters will be saved, current and target parameters will be shown[0m
 )
 if not defined quiet %pausecls%
 
 :: 1 - Hibernation enable and setup
 :: set Fast Startup ON
 set "Step=1.1/ (Re)-Activate Fast Startup"
-call :ProcessKey add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Power" "HiberbootEnabled" "reg_dword" 1 
+call :ProcessKey add "%RegKeyHeader%\Session Manager\Power" "HiberbootEnabled" "reg_dword" 1 
 :: show hibernate after options
 set "Step=1.2/ Add option 'Hibernation timeout' into legacy Power Settings advanced options"
-call :ProcessKey add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Power\PowerSettings\238C9FA8-0AAD-41ED-83F4-97BE242C8F20\9d7815a6-7ee4-497e-8888-515a05f02364" "Attributes" "reg_dword" 2
+call :ProcessKey add "%RegKeyHeader%\Power\PowerSettings\238C9FA8-0AAD-41ED-83F4-97BE242C8F20\9d7815a6-7ee4-497e-8888-515a05f02364" "Attributes" "reg_dword" 2
 :: Activer hibernation
 echo [93m1.3/ Activate Hibernation/Fast Startup : Sleep S0, hibernation, and Fast Startup will be available[0m
 if not defined admin echo [31mNO CHANGE performed[0m
@@ -64,21 +66,21 @@ if not defined quiet %pausecls%
 
 :: 2 - Disable Core Isolation
 set "Step=2/ Disable Core Isolation. After reboot, clic on 'Ignore' on yellow icon in 'Windows Sécurity'"
-call :ProcessKey add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\DeviceGuard\Scenarios\HypervisorEnforcedCodeIntegrity" "Enabled" "reg_dword" 0
+call :ProcessKey add "%RegKeyHeader%\DeviceGuard\Scenarios\HypervisorEnforcedCodeIntegrity" "Enabled" "reg_dword" 0
 
 :: 3 - Set 2 important Power Management registry keys
 set "Step=3.1 et 3.2/ Power Management : IO coalescing timeout set to 60s and policy for devices powering down while the system is running set to power saving
-call :ProcessKey add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Power\PowerSettings\2e601130-5351-4d9d-8e04-252966bad054\c36f0eb4-2988-4a70-8eee-0884fc2c2433\DefaultPowerSchemeValues\%actpowplanguid%" "ACSettingIndex" "reg_dword" 0x0000ea60
-call :ProcessKey add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Power\PowerSettings\4faab71a-92e5-4726-b531-224559672d19\DefaultPowerSchemeValues\%actpowplanguid%" "ACSettingIndex" "reg_dword" 1
+call :ProcessKey add "%RegKeyHeader%\Power\PowerSettings\2e601130-5351-4d9d-8e04-252966bad054\c36f0eb4-2988-4a70-8eee-0884fc2c2433\DefaultPowerSchemeValues\%actpowplanguid%" "ACSettingIndex" "reg_dword" 0x0000ea60
+call :ProcessKey add "%RegKeyHeader%\Power\PowerSettings\4faab71a-92e5-4726-b531-224559672d19\DefaultPowerSchemeValues\%actpowplanguid%" "ACSettingIndex" "reg_dword" 1
 
 :: 4 - Reconfigure nVidia HDA audio driver for Idle Times
 set "Step=4.1 et 4.2/ Modify Idle Time AC and DC for HDA nVidia driver"
-call :ProcessKey add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Class\{4d36e96c-e325-11ce-bfc1-08002be10318}\0003\PowerSettings" "ConservationIdleTime" "REG_BINARY" 00000000 
-call :ProcessKey add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Class\{4d36e96c-e325-11ce-bfc1-08002be10318}\0003\PowerSettings" "PerformanceIdleTime" "REG_BINARY" 00000000
+call :ProcessKey add "%RegKeyHeader%\Class\{4d36e96c-e325-11ce-bfc1-08002be10318}\0003\PowerSettings" "ConservationIdleTime" "REG_BINARY" 00000000 
+call :ProcessKey add "%RegKeyHeader%\Class\{4d36e96c-e325-11ce-bfc1-08002be10318}\0003\PowerSettings" "PerformanceIdleTime" "REG_BINARY" 00000000
 
 :: 5 - Modify TDR Delay to long value to avoid Winlogon or nvlddmkm.dll crashes while in Modern Standby 
 set "Step=5/ Modify Graphics drivers Tdr Delay"
-call :ProcessKey add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\GraphicsDrivers" "TdrDelay" "reg_dword" 60 
+call :ProcessKey add "%RegKeyHeader%\GraphicsDrivers" "TdrDelay" "reg_dword" 60 
 
 if defined quiet goto :eof
 if not defined admin goto :eof
