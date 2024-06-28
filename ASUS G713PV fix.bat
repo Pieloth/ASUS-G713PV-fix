6e7e84f12ee40f2d5eb8bd3fab2c6e7fffe5c479
Nicolas Martin <73445512+Pieloth@users.noreply.github.com>
2024-06-28 14:21:49 +0200

    Changed user/admin, reboot, auto detect GUID logics
    user mode and admin mode logics and messages changed. Propose reboot at the end. Autodetect active Power scheme GUID for some registry keys

--- ASUS G713PV fix.bat
+++ ASUS G713PV fix.bat
@@ -6,7 +6,7 @@ set quiet=
 set "p1=%1"
 if /I "%p1:~0,2%"=="/q" set quiet=1
 
-:: Retreive legacy active power mode GUID in 1st line of powercfg /q
+:: Retreive legacy active power scheme GUID from 1st line of powercfg /q
 for /f "delims=" %%i in ('powercfg /q') do set actpowplan=%%i & goto :stop
 :stop
 for /f "tokens=2 delims=:" %%i in ("%actpowplan%") do set actpowplan1=%%i
@@ -14,7 +14,6 @@ for /f "tokens=1" %%i in ("%actpowplan1%") do set actpowplanguid=%%i
 
 chcp 65001 > NUL
 
-if not defined quiet (
 echo [37;90m[38;5;233m@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 echo [38;5;234m@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 echo [38;5;235m@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@[38;5;124m#[38;5;203m(((((([38;5;088m\%[38;5;235m@@@@@@@@@@@@
@@ -39,16 +38,17 @@ echo This installer sets up some parameters and registries to best stabilize thi
 echo 
 echo Use also G Helper instead of Armoury Crate for even better stability
 echo:
-)
+
 :: Get admin status
 if exist %windir%\system32\config\systemprofile\* (
   echo  ]]] [42m[93m Admin rights ON =^> parameters will be applied[0m
   set admin=1
 ) else (
-  echo  [7m[91m]]] Admin rights OFF =^> No change or parameters will be saved[0m
+  echo  [7m[91m]]] Admin rights OFF =^> No change or parameters will be saved, current and target parameters will be shown[0m
 )
 if not defined quiet %pausecls%
-:: Hibernation enable and setup
+
+:: 1 - Hibernation enable and setup
 :: set Fast Startup ON
 set "Step=1.1/ (Re)-Activate Fast Startup"
 call :ProcessKey add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Power" "HiberbootEnabled" "reg_dword" 1 
@@ -56,46 +56,47 @@ call :ProcessKey add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Sessio
 set "Step=1.2/ Add option 'Hibernation timeout' into legacy Power Settings advanced options"
 call :ProcessKey add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Power\PowerSettings\238C9FA8-0AAD-41ED-83F4-97BE242C8F20\9d7815a6-7ee4-497e-8888-515a05f02364" "Attributes" "reg_dword" 2
 :: Activer hibernation
-
-echo [93m1.3/ Activate Hibernation/Fast Startup : Sleep S0, hibernation, and Fast Startup available[0m
+echo [93m1.3/ Activate Hibernation/Fast Startup : Sleep S0, hibernation, and Fast Startup will be available[0m
+if not defined admin echo [31mNO CHANGE performed[0m
 powercfg /h on
 powercfg /a | findstr /v "^$"
 if not defined quiet %pausecls%
 
-:: Disable Core Isolation
+:: 2 - Disable Core Isolation
 set "Step=2/ Disable Core Isolation. After reboot, clic on 'Ignore' on yellow icon in 'Windows Sécurity'"
 call :ProcessKey add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\DeviceGuard\Scenarios\HypervisorEnforcedCodeIntegrity" "Enabled" "reg_dword" 0
 
-:: Set 2 important Power Management registry keys
+:: 3 - Set 2 important Power Management registry keys
 set "Step=3.1 et 3.2/ Power Management : IO coalescing timeout set to 60s and policy for devices powering down while the system is running set to power saving
 call :ProcessKey add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Power\PowerSettings\2e601130-5351-4d9d-8e04-252966bad054\c36f0eb4-2988-4a70-8eee-0884fc2c2433\DefaultPowerSchemeValues\%actpowplanguid%" "ACSettingIndex" "reg_dword" 0x0000ea60
 call :ProcessKey add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Power\PowerSettings\4faab71a-92e5-4726-b531-224559672d19\DefaultPowerSchemeValues\%actpowplanguid%" "ACSettingIndex" "reg_dword" 1
 
-:: Reconfigure nVidia HDA audio driver for Idle Times
-set "Step=4.1 et 4.2/ Modify Idle Time AC et DC for HDA nVidia driver"
+:: 4 - Reconfigure nVidia HDA audio driver for Idle Times
+set "Step=4.1 et 4.2/ Modify Idle Time AC and DC for HDA nVidia driver"
 call :ProcessKey add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Class\{4d36e96c-e325-11ce-bfc1-08002be10318}\0003\PowerSettings" "ConservationIdleTime" "REG_BINARY" 00000000 
 call :ProcessKey add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Class\{4d36e96c-e325-11ce-bfc1-08002be10318}\0003\PowerSettings" "PerformanceIdleTime" "REG_BINARY" 00000000
 
-:: Modify TDR Delay to long value to avoid Winlogon or nvlddmkm.dll crashes while in Modern Standby 
+:: 5 - Modify TDR Delay to long value to avoid Winlogon or nvlddmkm.dll crashes while in Modern Standby 
 set "Step=5/ Modify Graphics drivers Tdr Delay"
 call :ProcessKey add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\GraphicsDrivers" "TdrDelay" "reg_dword" 60 
 
+if defined quiet goto :eof
+if not defined admin goto :eof
 echo:
 echo ]]]  Now, [6m[91mPlease REBOOT computer [0mto apply changes and parameters!
 echo:
-if defined quiet goto :eof
-set /p continue="Do you want to reboot computer in 30 sec (y/n): "
+set /p continue="Do you want to reboot computer in 30 seconds? (y/n): "
 if /I "%continue%" == "y" (
 	echo shutdown in 30 seconds.
 	shutdown /r
 )
 goto :eof
 
-:ProcessKey   - Processes the current key
+:ProcessKey   - Processes the current registry key
 ::cls
 echo [93m%step%[0m
 echo -------------------
-echo [34mRegistry key BEFORE change:[0m
+echo [34mCurrent Registry key BEFORE change:[0m
 reg query %2 /v %3 /t %4
 if errorlevel 1 (
 	echo [91mDID NOT FIND KEY %2 value %3 type %4
@@ -104,10 +105,15 @@ if errorlevel 1 (
 )
 echo:
 echo [36mRegistry key CHANGE STATUS:[0m
-if "%1" == "add" reg %1 %2 /v %3 /t %4 /d %5 /f
-if "%1" == "delete" reg %1 %2 /v %3 /f
+if not defined admin (
+	echo [31mNO CHANGE performed[0m
+) else (
+	if "%1" == "add" reg %1 %2 /v %3 /t %4 /d %5 /f
+	if "%1" == "delete" reg %1 %2 /v %3 /f
+)
 echo:
-echo [35mRegistry key AFTER change:[0m
+if defined admin echo [35mRegistry key AFTER change:[0m
+if not defined admin echo [35mRegistry key TARGET change:[0m
 reg query %2 /v %3 /t %4
 :EndProcessKey
 echo:
