e4226d05be6551027b974cc525b414af5d288b4a
Nicolas Martin <73445512+Pieloth@users.noreply.github.com>
2024-09-05 22:40:14 +0200

    New approach changes and simplifications
    Other than setting Hibernate and Fast startup options, only 2 registry keys are still ther for now. Under test. Good stability in this conditions

--- ASUS_G713PV_fix.bat
+++ ASUS_G713PV_fix.bat
@@ -48,27 +48,10 @@ for /f "tokens=1" %%i in ("%actpowplan1%") do set actpowplanguid=%%i
 
 set "RegKeyHeader=HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control"
 
-:: Retreive Media classes that need power Idle Time adjustment.
-:: See https://learn.microsoft.com/en-us/windows-hardware/drivers/audio/audio-device-class-inactivity-timer-implementation
-for /f "delims=" %%j in ('reg query "%RegKeyHeader%\Class\{4d36e96c-e325-11ce-bfc1-08002be10318}" /s /f "NVIDIA*Audio"') do set nVidiaHDA=%%j & goto :EndnVidiaHDA
-:EndnVidiaHDA
-for /f "delims=" %%j in ('reg query "%RegKeyHeader%\Class\{4d36e96c-e325-11ce-bfc1-08002be10318}" /s /f "Realtek*Audio"') do set Realtek=%%j & goto :EndRealtek
-:EndRealtek
-for /f "delims=" %%j in ('reg query "%RegKeyHeader%\Class\{4d36e96c-e325-11ce-bfc1-08002be10318}" /s /f "AMD Streaming Audio"') do set AMDstreaming=%%j & goto :EndAMDstreaming
-:EndAMDstreaming
-:: Remove spaces in strings
-set nVidiaHDA=%nVidiaHDA: =%
-set Realtek=%Realtek: =%
-set AMDstreaming=%AMDstreaming: =%
-
 if defined rollback (set hibernate=off) else (set hibernate=on)
-if defined rollback (set coreisolation=1) else (set coreisolation=0)
 if defined rollback (set policypwrdn=0) else (set policypwrdn=1)
 if defined rollback (set netACstby=1) else (set netACstby=0)
-if defined rollback (set PwrIdleState=03000000) else (set PwrIdleState=00000000)
-if defined rollback (set nvidletime=04000000) else (set nvidletime=00000000)
-if defined rollback (set amdidletime=03000000) else (set amdidletime=00000000)
-if defined rollback (set RB=ROLLBACK:) else (set RB=EXECUTE:)
+f defined rollback (set RB=ROLLBACK:) else (set RB=EXECUTE:)
 if defined rollback echo [6m[91mROLLBACK PROCEDURE TO DEFAULTS WILL BE APPLIED TO REGISTRY ENTRIES[0m
 
 :: Get admin status
@@ -80,44 +63,22 @@ if exist %windir%\system32\config\systemprofile\* (
 )
 if not defined quiet %pausecls%
 
-:: 1 - Hibernation enable and setup
-:: set Fast Startup ON (not rollbacked)
-set "Step=1.1/ (Re)-Activate Fast Startup"
-call :ProcessKey add "%RegKeyHeader%\Session Manager\Power" "HiberbootEnabled" "REG_DWORD" 1 
 :: show hibernate after options (not rollbacked)
-set "Step=1.2/ Add option 'Hibernation timeout' into legacy Power Settings advanced options"
+set "Step=1/ Add option 'Hibernation timeout' into legacy Power Settings advanced options"
 call :ProcessKey add "%RegKeyHeader%\Power\PowerSettings\238C9FA8-0AAD-41ED-83F4-97BE242C8F20\9d7815a6-7ee4-497e-8888-515a05f02364" "Attributes" "REG_DWORD" 2
 :: Enable/disable hibernation. Rollback will disable also 1.1 and 1.2
-echo [93m1.3/ %RB% Enable Hibernation/Fast Startup : Sleep S0, hibernation, and Fast Startup will be available[0m
+echo [93m2/ %RB% Enable Hibernation/Fast Startup : Sleep S0, hibernation, and Fast Startup will be available[0m
 powercfg /h %hibernate%
 if not defined admin echo [31mNO CHANGE performed. Current Power configuration is:[0m
 powercfg /a | findstr /v "^$"
 if not defined quiet %pausecls%
 
-:: 2 - Disable Core Isolation
-set "Step=2/ %RB% Disable Core Isolation. After reboot, clic on 'Ignore' on yellow icon in 'Windows Sécurity'"
-call :ProcessKey add "%RegKeyHeader%\DeviceGuard\Scenarios\HypervisorEnforcedCodeIntegrity" "Enabled" "REG_DWORD" %coreisolation%
-
 :: 3 - Set important Power Management registry keys
-set "Step=3.1/ %RB% policy for devices powering down while the system is running (power saving for AC and DC)"
+set "Step=3/ %RB% policy for devices powering down while the system is running (power saving for AC and DC)"
 call :ProcessKey add "%RegKeyHeader%\Power\PowerSettings\4faab71a-92e5-4726-b531-224559672d19\DefaultPowerSchemeValues\%actpowplanguid%" "ACSettingIndex" "REG_DWORD" %policypwrdn%
 :: STRONGLY RECOMMENDED: Disable networking in standby in AC and/or DC for more quiet Modern Standby sleep!
-set "Step=3.2/ %RB% Networking connectivity in Standby (Disable networking in Standby for AC.)"
+set "Step=4/ %RB% Networking connectivity in Standby (Disable networking in Standby for AC.)"
 call :ProcessKey add "%RegKeyHeader%\Power\PowerSettings\f15576e8-98b7-4186-b944-eafa664402d9\DefaultPowerSchemeValues\%actpowplanguid%" "ACSettingIndex" "REG_DWORD" %netACstby%
-
-:: 4 - Force Idle states to D0 for nVidia HDA, AMD and Realtek audio drivers. Note Realtek forces Idle times, not Idle power state
-set "Step=4.1/ %RB% Force Idle Power State to D0 in AC and DC for AMD Streaming Audio driver"
-call :ProcessKey add "%AMDstreaming%\PowerSettings" "IdlePowerState" "REG_BINARY" %PwrIdleState%
-set "Step=4.2 and 4.3/ %RB% Disable Idle Time AC and DC for AMD Streaming Audio driver"
-call :ProcessKey add "%AMDstreaming%\PowerSettings" "ConservationIdleTime" "REG_BINARY" %amdidletime% 
-call :ProcessKey add "%AMDstreaming%\PowerSettings" "PerformanceIdleTime" "REG_BINARY" %amdidletime%
-set "Step=5.1/ %RB% Force Idle Power State to D0 in AC and DC for nVidia HDA driver"
-call :ProcessKey add "%nVidiaHDA%\PowerSettings" "IdlePowerState" "REG_BINARY" %PwrIdleState%
-set "Step=5.2 and 5.3/ %RB% Disable Idle Time AC and DC for nVidia HDA driver"
-call :ProcessKey add "%nVidiaHDA%\PowerSettings" "ConservationIdleTime" "REG_BINARY" %nvidletime% 
-call :ProcessKey add "%nVidiaHDA%\PowerSettings" "PerformanceIdleTime" "REG_BINARY" %nvidletime%
-set "Step=6/ %RB% Force Idle Power State to D0 in AC and DC for Realtek Audio driver"
-call :ProcessKey add "%Realtek%\PowerSettings" "IdlePowerState" "REG_BINARY" %PwrIdleState%
 
 if defined quiet goto :eof
 if not defined admin goto :eof
